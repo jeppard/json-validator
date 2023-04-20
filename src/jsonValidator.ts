49d6323a8f895644f599ebc33901481ca02c2225
jeppard <jonas.eppard@gmail.com>
2023-04-20 15:16:03 +0200

    Added schema Validation

--- src/jsonValidator.ts
+++ src/jsonValidator.ts
@@ -1,7 +1,6 @@
 export class JSONValidator {
     validate = () => {};
-    updateSchema = (schema: any) => {
-        this.schema = schema;
+    private checkSchema = (schema: any) => {
         if (!schema.hasOwnProperty("type")) {
             throw new Error("Schema must have a type property");
         }
@@ -9,15 +8,14 @@ export class JSONValidator {
             throw new Error("Schema type must be string, number, boolean, object, or array");
         }
     };
+    updateSchema = (schema: any) => {
+        this.checkSchema(schema);
+        this.schema = schema;
+    };
     schema: any;
     constructor(schema: any = {}) {
+        this.checkSchema(schema);
         this.schema = schema;
-        if (!schema.hasOwnProperty("type")) {
-            throw new Error("Schema must have a type property");
-        }
-        if (["string", "number", "boolean", "object", "array"].indexOf(schema.type) === -1) {
-            throw new Error("Schema type must be string, number, boolean, object, or array");
-        }
     }
 }
 
