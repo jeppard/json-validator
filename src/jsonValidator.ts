ed774b01e973be70c4c056df2ef50ce90b00bcb2
jeppard <jonas.eppard@gmail.com>
2023-04-20 14:56:12 +0200

    Added Update Schema

--- src/jsonValidator.ts
+++ src/jsonValidator.ts
@@ -1,6 +1,11 @@
 export class JSONValidator {
     validate = () => {};
+    updateSchema = (schema: any) => {
+        this.schema = schema;
+    };
+    schema: any;
     constructor(schema: any = {}) {
+        this.schema = schema;
     }
 }
 
--- test/jsonValidator.spec.ts
+++ test/jsonValidator.spec.ts
@@ -13,9 +13,18 @@ describe("JSON Validator", () => {
     it("should have a validate method", () =>{
         expect(jsonValidator).to.have.property("validate").that.is.a("function");
     });
-    it("should take a schema as a parameter", () => {
+    it("should take a schema as a parameter and store it", () => {
         let schema = {};
         let validator = new JSONValidator(schema);
-        expect(validator).to.exist;
+        expect(validator.schema).to.equal(schema);
+        schema = { "type": "string" };
+        validator = new JSONValidator(schema);
+        expect(validator.schema).to.equal(schema);
+    });
+    it("should have a updateSchema method", () => {
+        expect(jsonValidator).to.have.property("updateSchema").that.is.a("function");
+        let schema = { "type": "string" };
+        jsonValidator.updateSchema(schema);
+        expect(jsonValidator.schema).to.equal(schema);
     });
 });
